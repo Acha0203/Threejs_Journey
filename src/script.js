b8f6d01bbc6cc1b7b20a30858b0a3ac0d9e33d20
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2022-11-12 01:12:01 +0900

    feat: completed lesson 06 in chapter01

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import './style.css';
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 // Scene
 const scene = new THREE.Scene();
@@ -77,9 +78,23 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
 // Animate
-const tick = () => {
-  console.log('tick');
+// const clock = new THREE.Clock();
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
 
+const tick = () => {
+  // Time
+  // const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  // mesh.rotation.y = elapsedTime;
+  // camera.position.x = Math.cos(elapsedTime);
+  // camera.position.y = Math.sin(elapsedTime);
+  // camera.lookAt(mesh.position);
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
   window.requestAnimationFrame(tick);
 };
 
