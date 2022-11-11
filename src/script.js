84bea25301d14928ebd199949aaf296a26cabf18
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2022-11-11 23:47:57 +0900

    feat: completed lesson 05 in chapter01

--- src/script.js
+++ src/script.js
@@ -6,9 +6,55 @@ const scene = new THREE.Scene();
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshNormalMaterial();
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+// Position
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+
+// mesh.position.set(0.7, -0.6, 1);
+
+// Scale
+// mesh.scale.set(2, 0.25, 0.5);
+
+// Rotation
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
+
+// Group
+// const group = new THREE.Group();
+// group.scale.y = 2;
+// group.rotation.y = 0.2;
+// scene.add(group);
+
+// const cube1 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshNormalMaterial()
+// );
+// cube1.position.x = -1.5;
+// group.add(cube1);
+
+// const cube2 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshNormalMaterial()
+// );
+// cube2.position.x = 0;
+// group.add(cube2);
+
+// const cube3 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshNormalMaterial()
+// );
+// cube3.position.x = 1.5;
+// group.add(cube3);
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 
 // Sizes
 const sizes = {
@@ -19,6 +65,7 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 scene.add(camera);
 
 // Renderer
@@ -28,3 +75,12 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+// Animate
+const tick = () => {
+  console.log('tick');
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
